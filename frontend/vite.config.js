76db3249ec6d21b3cf0254693be416407304a7fb
Cecep Nandang <cecep@kima.one>
2026-02-25 23:06:53 +0700

    feat: init app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://api-inventory.isavralabel.com/simokardi',
         changeOrigin: true,
       },
     },
